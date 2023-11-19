2a3ba17c4d6011205359fd4ff44eada76076d300
Siyamon69 <mir10siam@gmail.com>
2023-11-19 21:18:41 +0600

    kk

--- ES6pp/destructuring.js
+++ ES6pp/destructuring.js
@@ -1,5 +1,15 @@
+<<<<<<< HEAD:ES6pp/destructuring.js
 const numbers = [1,34,53,545,33]
 
 const [first , second] = numbers
 
 console.log(second,second,first,second)
+=======
+const numbers = [1,34,53,545,33]
+
+const [first , second] = numbers
+
+console.log(second,second,first,second)
+console.log(second,second,first,second)
+console.log("Destructuring")
+>>>>>>> e7f38f5 (kk):destructuring.js
